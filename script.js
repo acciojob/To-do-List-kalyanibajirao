5005281d490fcea91eafb4dab4221d4d2e13e79b
Kalyani Bajirao <95849305+kalyanibajirao@users.noreply.github.com>
2024-05-16 13:56:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
 
+document.getElementById('addTodoBtn').addEventListener('click', () => {
+  const todoText = inputField.value.trim(); 
+  if (todoText) {
+    const listItem = document.createElement('li');
+    listItem.textContent = todoText;
+
+    todoList.appendChild(listItem);
+
+    // Clear the input field
+    inputField.value = '';
+  }
+});
